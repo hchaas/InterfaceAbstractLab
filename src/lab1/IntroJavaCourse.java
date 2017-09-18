7ce2888b517e09ec70ea4870706e38869ba9b8d4
User <User@192.168.0.7>
2017-09-17 20:17:01 -0500

    final cleanup of lab 1

--- src/lab1/IntroJavaCourse.java
+++ src/lab1/IntroJavaCourse.java
@@ -6,5 +6,4 @@ public class IntroJavaCourse extends AdvancedJavaCourse{
         super(courseName, courseNumber);
     }
 
-
 }
--- src/lab1/Start.java
+++ src/lab1/Start.java
@@ -3,15 +3,17 @@ package lab1;
 
 public class Start {
     public static void main(String[] args) {
-        
+        /*Using the Liskov principle makes sense to me when you're creating a list of objects
+        to do the same thing, assuming that thing is an ability/method of the superclass
+        */
         ProgrammingCourse [] courses = {
             new IntroToProgrammingCourse("Introduction to Programming", "43534"),
             new IntroJavaCourse("Introduction to Java Programming", "3433"),
             new AdvancedJavaCourse("Advanced Java Programming", "9895")
         };
         
-        for (int i =0; i <courses.length; i++){
-            System.out.println(courses[i].getCourseName());
+        for (ProgrammingCourse course : courses) {
+            System.out.println(course.getCourseName());
         }
     }
 
